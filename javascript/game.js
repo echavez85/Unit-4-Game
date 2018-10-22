b0ebfc848bc06c128f3437f0a63e164bff5b4256
echavez85 <echavez85@gmail.com>
2018-10-22 18:31:35 -0500

    Pseudocoded game.js

---
+++ javascript/game.js
@@ -0,0 +1,10 @@
+// Function to select a random number between 5 and 75 and place in #randomNumber.
+
+
+// Function to count wins vs. losses.
+
+// Function will assign a random number between 1 and 9 to each of the Golden Girls. This number will change every "new game". This will probably be like the numbers on a calculator, an addition symbol, and equal sign all combined so that numbers can add up.
+
+// This will add up/display the total value of the Golden Girls in the same way the result area did on the calculator.
+
+// If result above is higher than random number generated above, loss will increase. If it's lower, nothing done. If it's equal, wins will increase.
