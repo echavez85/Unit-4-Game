a09e88d423954ad67f49bcb26a77630485f9f5dd
echavez85 <echavez85@gmail.com>
2018-10-26 23:47:53 -0500

    Redid JS. Created variables and num gen functions.

--- javascript/game.js
+++ javascript/game.js
@@ -1,25 +1,52 @@
-// Make variables global to the runtime of the application and initialize the game.
+// Set variables to 0 at document loading
 $(document).ready(function() {
 
     var wins = 0;
     var losses = 0;
-    var randomNumber = "";
+    var targetNumber = "";
     var counter = 0;
-    var dorothy = "";
-    var rose = "";
-    var sophia = "";
-    var blanche = "";
+    var dorothyNumber = "";
+    var roseNumber = "";
+    var blancheNumber = "";
+    var sophiaNumber = "";
+    
 
 // Upon game initialization, random number between 19 and 120 should be assigned to #randomNumber div. Random number between 1 and 12 should be assigned to each of the Golden Girls images.
-    function generateRandomNumbers () {
-        randomNumber = Math.floor(Math.random() * 102) + 19;
-        dorothy = Math.floor(Math.random() * 12) + 1;
-        rose = Math.floor(Math.random() * 12) + 1;
-        sophia = Math.floor(Math.random() * 12) + 1;
-        blanch = Math.floor(Math.random() * 12) + 1;
+   function generateTargetNumber () {
+        return Math.floor(Math.random() * 102) + 19;
+   }
+   
+   var beginGame = function() {
+       counter = 0;
+       targetNumber = generateTargetNumber();
+       dorothyNumber = generateGirlsNumbers();
+       roseNumber = generateGirlsNumbers();
+       blancheNumber = generateGirlsNumbers();
+       sophiaNumber = generateGirlsNumbers();
+
+       $("#randomNumber").text(targetNumber);
+       $("#goldenGirlsScore").text(counter);
+
+   };
+
+   function generateGirlsNumbers () {
+        return Math.floor(Math.random() * 12) + 1;
+   }
 
 
-    })
+//Functions for each of the Golden Girls to get a number assigned when clicked on. Click event should also check to see if Golden Girls Score is equal to or greater than Target Number.
+   $("#dorothy").on("click", function() {
+       counter += dorothyNumber;
+       $("#goldenGirlsScore").text(counter);
+       checkWin();
+
+   })
+   
+
+})
+        
+        
+
 
 
 // Add up the amount that the Golden Girls add up to and calculate in #goldenGirlsScore div. Compare to randomNumber. If sum is equal to random numbers, increase wins and restart. If sum is greater, increase losses and restart. 
